dfe0e1c0023863b8fd0c8f26c208400b66dea845
wilig <wilig@4dc5e89f-90f6-0310-ab54-a6a856e7c30e>
2007-06-05 17:06:30 +0000

    Temp branch for diagnosing rsingers problem
    git-svn-id: svn://rubyforge.org/var/svn/oai@855 4dc5e89f-90f6-0310-ab54-a6a856e7c30e

---
+++ ruby-oai/branches/rsinger/test/activerecord_provider/helpers/providers.rb
@@ -0,0 +1,46 @@
+require 'active_record'
+require 'oai'
+require "config/connection"
+require "helpers/custom_metadata_format"
+
+Dir.glob(File.dirname(__FILE__) + "/../models/*.rb").each do |lib|
+  require lib
+end
+
+class ARProvider < OAI::Provider::Base
+  repository_name 'ActiveRecord Based Provider'
+  repository_url 'http://localhost'
+  record_prefix 'oai:test'
+  source_model ActiveRecordWrapper.new(DCField)
+  register_format CustomMetadataFormat.instance
+end
+
+class SimpleResumptionProvider < OAI::Provider::Base
+  repository_name 'ActiveRecord Resumption Provider'
+  repository_url 'http://localhost'
+  record_prefix 'oai:test'
+  source_model ActiveRecordWrapper.new(DCField, :limit => 25)
+end
+
+class CachingResumptionProvider < OAI::Provider::Base
+  repository_name 'ActiveRecord Caching Resumption Provider'
+  repository_url 'http://localhost'
+  record_prefix 'oai:test'
+  source_model ActiveRecordCachingWrapper.new(DCField, :limit => 25)
+end
+  
+
+class ARLoader
+  def self.load
+    fixtures = YAML.load_file(
+      File.join(File.dirname(__FILE__), '..', 'fixtures', 'dc.yml')
+    )
+    fixtures.keys.sort.each do |key|
+      DCField.create(fixtures[key])
+    end
+  end
+  
+  def self.unload
+    DCField.delete_all
+  end
+end
---
+++ ruby-oai/branches/rsinger/test/activerecord_provider/models/dc_field.rb
@@ -0,0 +1,12 @@
+class DCField < ActiveRecord::Base
+  set_inheritance_column 'DONOTINHERIT'
+  has_and_belongs_to_many :sets, 
+    :join_table => "dc_fields_dc_sets", 
+    :foreign_key => "dc_field_id", 
+    :class_name => "DCSet"
+    
+  def to_cmf
+    "<cmf:title>This is the title</cmf:title>"
+  end
+  
+end
